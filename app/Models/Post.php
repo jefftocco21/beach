13820ec9f08fd9c4f992d913dc373f7ce9e5e9f7
Jeff Tocco <jnt369@gmail.com>
2023-08-17 12:35:37 -0400

    Route model binding

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,5 +10,6 @@ class Post extends Model
     use HasFactory;
     
     // protected $guarded = ['id'];
-    protected $fillable = ['title', 'excerpt', 'body'];
+    protected $fillable = ['title', 'excerpt', 'body', 'slug'];
+
 }
