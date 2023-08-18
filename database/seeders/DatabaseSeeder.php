634c507a97300e1eb491e8068f6306fa4a649b68
Jeff Tocco <jnt369@gmail.com>
2023-08-17 21:28:28 -0400

    Seeding the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::factory()->create();
+        
+        $fourByFour = Category::create([
+            'name' => '4x4',
+            'slug' => '4x4'
+        ]);
+
+        $regular = Category::create([
+            'name' => 'Regular Beach',
+            'slug' => 'Regular Beach',
+        ]);
+
+        $handicap = Category::create([
+            'name' => 'Handicap',
+            'slug' => 'Handicap'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $fourByFour->id,
+            'title' => 'Four By Four Post',
+            'slug' => 'Four By Four Post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $handicap->id,
+            'title' => 'Handicap Post',
+            'slug' => 'Handicap Post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+        ]);
     }
 }
+
