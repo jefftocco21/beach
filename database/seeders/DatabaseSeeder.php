21a846c3a79e17fde91078e5d730ebe610109f84
Jeff Tocco <jnt369@gmail.com>
2023-08-17 22:15:25 -0400

    Viewing all posts by a user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,40 +20,50 @@ class DatabaseSeeder extends Seeder
         Category::truncate();
         Post::truncate();
 
-        $user = User::factory()->create();
+
+        $user = User::factory()->create([
+            'name' => 'Jeff Tocco'
+        ]);
+
+        Post::factory(5)->create([
+            'user_id' => $user->id,
+        ]);
+
+
+        // $user = User::factory()->create();
         
-        $fourByFour = Category::create([
-            'name' => '4x4',
-            'slug' => '4x4'
-        ]);
+        // $fourByFour = Category::create([
+        //     'name' => '4x4',
+        //     'slug' => '4x4'
+        // ]);
 
-        $regular = Category::create([
-            'name' => 'Regular Beach',
-            'slug' => 'Regular Beach',
-        ]);
+        // $regular = Category::create([
+        //     'name' => 'Regular Beach',
+        //     'slug' => 'Regular Beach',
+        // ]);
 
-        $handicap = Category::create([
-            'name' => 'Handicap',
-            'slug' => 'Handicap'
-        ]);
+        // $handicap = Category::create([
+        //     'name' => 'Handicap',
+        //     'slug' => 'Handicap'
+        // ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $fourByFour->id,
-            'title' => 'Four By Four Post',
-            'slug' => 'Four By Four Post',
-            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-            'body' => "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
-        ]);
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $fourByFour->id,
+        //     'title' => 'Four By Four Post',
+        //     'slug' => 'Four By Four Post',
+        //     'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+        //     'body' => "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+        // ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $handicap->id,
-            'title' => 'Handicap Post',
-            'slug' => 'Handicap Post',
-            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-            'body' => "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
-        ]);
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $handicap->id,
+        //     'title' => 'Handicap Post',
+        //     'slug' => 'Handicap Post',
+        //     'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+        //     'body' => "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+        // ]);
     }
 }
 
