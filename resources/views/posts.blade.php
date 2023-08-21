a9adfccdc1b81f8fd432d90871083b4128ead483
Jeff Tocco <jnt369@gmail.com>
2023-08-21 12:40:46 -0400

    Creating a grid component with CSS for feature, 2, 3

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -15,18 +15,15 @@
     </article>
     @endforeach --}}
 
-    @include('_posts-header');
+    @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <x-post-featured :post="$posts[0]" />
 
-
-            <div class="lg:grid lg:grid-cols-2">
-                @foreach ($posts->skip(1) as $post)
-                <x-post-card :post="$post" />
-                @endforeach
-            </div>
-
+        @if($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p class="text-center">No posts yet. Please check back in later on!</p>
+        @endif
         
     </main>
 </x-layout>
