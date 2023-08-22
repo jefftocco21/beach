4a138c6870583224f06b84ca52e57e96704b6bdd
Jeff Tocco <jnt369@gmail.com>
2023-08-22 17:33:15 -0400

    Javascript dropdown with Alpine

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,8 @@ Route::get('/', function () {
     });
 
     return view('posts', [
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -39,12 +40,15 @@ Route::get('posts/{post:slug}', function(Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCat' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
