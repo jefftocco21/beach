781a746ff0df9c6165b43d90d7b50e9667829f92
Jeff Tocco <jnt369@gmail.com>
2023-08-29 17:29:01 -0400

    dynamic comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function author() //thinks by default that foreign key is author_id
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
+
