c84ac7295edfbe54359ab13429f33dc5bbca294f
Jeff Tocco <jnt369@gmail.com>
2023-08-29 12:26:11 -0400

    hardcoded comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,6 +50,14 @@
                             {!!$post->body!!}
                         </div>
                     </div>
+
+                    <section class="col-start-5 col-span-8 mt-10 space-y-6">
+                        <x-post-comment />
+                        <x-post-comment />
+                        <x-post-comment />
+                        <x-post-comment />
+                    </section>
+                
                 </article>
             </main>
         </section>
