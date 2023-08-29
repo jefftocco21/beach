2259181c007b08a7773d8e3e52d8c77844ad9c01
Jeff Tocco <jnt369@gmail.com>
2023-08-28 21:25:26 -0400

    Login page

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -11,5 +13,30 @@ class SessionController extends Controller
         auth()->logout();
 
         return redirect('/')->with('success', 'Logout successful, goodbye!');
+    }
+
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        //validate and try to loguse in
+        $attributes = request()->validate([ //bad
+            'email' => ['required','email'],
+            'password' => ['required']
+        ]);
+
+        //if the auth faied
+        throw ValidationException::withMessages([ //bad
+            'email' => 'Provided credentials could not be verified.'
+        ]);
+
+        if(auth()->attempt($attributes)) //good
+        {
+            //successful redirect
+            return redirect('/')->with('success', 'Login succesful. Welcome back!');
+        }
     }
 }
