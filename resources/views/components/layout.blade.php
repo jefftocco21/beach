271fd6b54e7a58d66ffb9a465afac97e4d21f8bf
Jeff Tocco <jnt369@gmail.com>
2023-08-29 22:08:32 -0400

    Logo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
         <nav class="md:flex md:justify-between md:items-center">
             <div>
                 <a href="/">
-                    <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                    <img src="" alt="Blog Project" width="165" height="16">
                 </a>
             </div>
 
