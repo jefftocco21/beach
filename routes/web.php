c73823cbf0774a6ab95d0f036871445351272ddb
Jeff Tocco <jnt369@gmail.com>
2023-08-30 18:29:15 -0400

    Unable to subscribe message

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,9 @@ Route::get('login', [SessionController::class, 'create'])->middleware('guest');
 Route::post('sessions', [SessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
 
-Route::get('ping', function () {
+Route::post('newsletter', function () {
+    request()->validate(['email' => 'required|email']);
+
     $mailchimp = new \MailchimpMarketing\ApiClient();
 
     $mailchimp->setConfig([
@@ -44,11 +46,18 @@ Route::get('ping', function () {
         'server' => 'us21'
     ]);
 
-    $response = $mailchimp->lists->addListMember('b13eb00a1d', [
-        'email_address' => 'joshcadorette96@gmail.com',
-        'status' => 'subscribed'
+    try {
+        $response = $mailchimp->lists->addListMember('b13eb00a1d', [
+            'email_address' => request('email'),
+            'status' => 'subscribed'
+        ]);
+    } catch (\Exception $e){
+        throw \Illuminate\validation\ValidationException::withMessages([
+            'email' => 'Sorry, this email could not be added to newsletter.'
+        ]);
+    }
     
-    ]);
+    return redirect('/')->with('success', 'You are now signed up for the newsletter!');
  
     dd($response);
 });
