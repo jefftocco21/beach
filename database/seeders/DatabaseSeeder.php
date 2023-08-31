71a179ec9a3632f9b890c991aff35f679cad5339
Jeff Tocco <jnt369@gmail.com>
2023-08-30 21:19:14 -0400

    Dynamic thumbnails

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
+        // User::truncate();
+        // Category::truncate();
+        // Post::truncate();
 
 
         $user = User::factory()->create([
